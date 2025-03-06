c95eb16917507d4bf7d43760d5c1a2506e65ff94
AlinaGladkova <tina.leaf89@gmail.com>
2025-03-06 20:31:03 +0300

    the rating is changed so the key issue could be resolved. in this case we added cloneelement method.

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -9,33 +9,19 @@ import React from "react";
 export default function Rating({ defaultRating, maxValue }) {
   const generateStars = (rating) => {
     const fullStars = Math.trunc(rating);
-    // const halfStar = rating % 1 >= 0.5 ? 1 : 0; //переделать. чтобы не возвращал ноль
-    const halfStar = rating % 1 >= 0.5 ? 1 : {}; //переделать. чтобы не возвращал ноль
-
+    const halfStar = rating % 1 >= 0.5;
     const emptyStars = maxValue - fullStars - (halfStar ? 1 : 0);
-
-    //через один массив и push, чтоб можно было map и добавить key
-    const arr = []
-      .concat(...[...Array(fullStars)].fill(<IoMdStar color="rgb(255, 166, 0)" />))
-      .concat(halfStar && <IoMdStarHalf color="rgb(255, 166, 0)" />)
-      .concat(...[...Array(emptyStars)].fill(<IoIosStarOutline color="rgb(255, 166, 0)" />));
-    console.log(arr);
 
     return (
       <>
-        {/* {[...Array(fullStars)].fill(<IoMdStar color="rgb(255, 166, 0)" />)}
-        {halfStar ? <IoMdStarHalf color="rgb(255, 166, 0)" /> : null}
-        {[...Array(emptyStars)].fill(<IoIosStarOutline color="rgb(255, 166, 0)" />)} */}
-
         {[]
           .concat(...[...Array(fullStars)].fill(<IoMdStar color="rgb(255, 166, 0)" />))
-          // .concat(halfStar && <IoMdStarHalf color="rgb(255, 166, 0)" />)
+          .concat(halfStar ? <IoMdStarHalf color="rgb(255, 166, 0)" /> : <></>)
           .concat(...[...Array(emptyStars)].fill(<IoIosStarOutline color="rgb(255, 166, 0)" />))
           .map((child, i) => {
-            return React.cloneElement(child);
-
-            // console.log(222, test);
-            // return cloneElement(child);
+            return cloneElement(child, {
+              key: i,
+            });
           })}
       </>
     );
